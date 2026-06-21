48d00015297395a7bbfca7bd6622b2544082785b
hrittik702 <mmhrittik@gmail.com>
2026-06-21 17:35:07 +0530

    Add : Lenis Scroll

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Initialize Lenis
-const lenis = new Lenis();
+const lenis = new Lenis({
+  autoRaf: true,
+});
 
-// Use requestAnimationFrame to continuously update the scroll
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
