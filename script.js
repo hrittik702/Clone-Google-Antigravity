8157120f9ed347f3c376ae093fe8e0e05f678b17
hrittik702 <mmhrittik@gmail.com>
2026-06-24 02:38:16 +0530

    vercel update

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Initialize Lenis
-const lenis = new Lenis({
-  autoRaf: true,
-});
+const lenis = new Lenis();
 
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
